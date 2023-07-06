1519807e10939d2035e3f73b231136bb007a8a25
aaisha <ranjitaaisha80@gmail.com>
2023-07-06 14:48:24 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { HomePage } from "./components/HomePage/HomePage";
 import { NavBar } from "./components/NavBar/NavBar";
+import { ProductPage } from "./components/ProductPage/ProductPage";
 import { SearchPage } from "./components/SearchPage/SearchPage";
 import { queryClient } from "./queryClient";
-import { ProductPage } from "./components/ProductPage/ProductPage";
 
 function App() {
   return (
